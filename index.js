605fbbcd87dba66dabd9f58d012af199568ca609
Adrian030304 <adrianromanenco1@gmail.com>
2024-07-04 21:22:35 +0300

    implemented the main functions which will help with the display of the info based on the api

--- index.js
+++ index.js
@@ -3,3 +3,20 @@
 const weatherForm = document.querySelector(".weatherForm");
 const cityInput = document.querySelector(".cityInput");
 const card = document.querySelector(".card")
+const apiKey = "e4f487901b6fa553bee9b703001ccc97"
+
+weatherForm.addEventListener("submit", event => {
+
+});
+
+async function getWeatherData(city){
+
+}
+
+function displayWeatherInfo(data) {
+
+}
+
+function getWeatherEmoji(weatherId){
+
+}
