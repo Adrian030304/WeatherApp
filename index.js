8a1c9e1c3be38f1bddab7403053ed9319ee8e78f
Adrian030304 <adrianromanenco1@gmail.com>
2024-07-04 22:33:28 +0300

    switch for every weather emoji with its code

--- index.js
+++ index.js
@@ -86,7 +86,19 @@ function getWeatherEmoji(weatherId){
 
     switch(true){
         case (weatherId >=200 && weatherId <300):
-            return "🌩️"
+            return "⛈️"
+        case (weatherId >=300 && weatherId <400):
+            return "🌧️"
+        case (weatherId >=500 && weatherId <600):
+            return "🌧️"
+        case (weatherId >=600 && weatherId <700):
+            return "❄️"
+        case (weatherId >=700 && weatherId <800):
+            return "🌫️"
+        case (weatherId === 800):
+            return "☀️"
+        case (weatherId ==> 801 && weatherId < 810):
+            return "☁️"
     }
 
 }
